705ddb50adb175819af1b0e6c4e4ba57ad8ec6c9
Masaki Hara <ackie.h.gmai@gmail.com>
2021-08-14 17:11:26 +0900

    Export Marshal object

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,6 +7,12 @@ describe("parse", () => {
   });
 });
 
+describe("Marshal.parse", () => {
+  it("parses a Buffer", () => {
+    expect(parse(Buffer.from([4, 8, 105, 47]))).toBe(42);
+  });
+});
+
 describe("MarshalError", () => {
   it("should be an instance of Error", () => {
     expect(new MarshalError()).toBeInstanceOf(Error);
